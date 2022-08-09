a880bde25a51227b95cfb32c8368ce13be4dc00c
kevin_ud <1308098049@qq.com>
2022-08-10 00:43:41 +0800

    增加中断标志保护，防止向下溢出

--- Src/stm32f1xx_it.c
+++ Src/stm32f1xx_it.c
@@ -223,7 +223,8 @@ void TIM1_UP_IRQHandler(void)
   /* USER CODE END TIM1_UP_IRQn 0 */
   HAL_TIM_IRQHandler(&htim1);
   /* USER CODE BEGIN TIM1_UP_IRQn 1 */
-	DMA_flag--;
+	if(DMA_flag)
+        DMA_flag--;
   /* USER CODE END TIM1_UP_IRQn 1 */
 }
 
